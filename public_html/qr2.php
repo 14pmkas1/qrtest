98853c0e0e2fa4b22a9e3f2d3efa27c1dae6fac7
14pmkas1 <karoza.spam@gmail.com>
2016-09-28 19:20:31 +0300

    First Version
    Very bad code

---
+++ public_html/qr2.php
@@ -0,0 +1,12 @@
+<?php
+$f=fopen("stat2.dat","a+"); 
+flock($f,LOCK_EX); 
+$count=fread($f,100); 
+@$count++; 
+ftruncate($f,0); 
+fwrite($f,$count); 
+fflush($f); 
+flock($f,LOCK_UN); 
+fclose($f);
+header( 'Location: http://wikilink.by' );
+?>
---
+++ public_html/qrview.php
@@ -0,0 +1,15 @@
+<?php 
+
+$s1=file("stat1.dat"); 
+echo "QR1: $s1[0]";
+echo "<br>";
+
+$s2=file("stat2.dat"); 
+echo "QR2: $s2[0]";
+echo "<br>";
+
+$s3=file("stat3.dat"); 
+echo "QR3: $s3[0]";
+echo "<br>";
+
+?> 
